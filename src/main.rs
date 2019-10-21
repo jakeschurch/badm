04cf5e8b79499f222140f9f79b1b2230c279eb91
Jake Schurch <jakeschurch@gmail.com>
2019-10-21 17:30:19 -0400

    feat: Create Config struct
    Signed-off-by: Jake Schurch <jakeschurch@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![allow(clippy::all)]
+
+/// Currently, the code sources the location of the dotfiles directory set by BADM
+/// in the env variable "BADM_DIR"
+const BADM_DIR_VAR: &str = "BADM_DIR";
+
 fn main() {
     println!("Hello, world!");
+}
+struct Config {}
+
+impl Config {
+    fn get_dots_dir() -> Option<String> {
+        match env::var(BADM_DIR_VAR) {
+            Ok(location) => Some(location),
+            Err(_) => None,
+        }
+    }
+
+    fn set_dots_dir<K: AsRef<OsStr>>(location: K) {
+        env::set_var(BADM_DIR_VAR, location)
+    }
+}
 }
