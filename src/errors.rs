c23cd1df24159220200430157626d997372d5a84
Jake Schurch <jakeschurch@gmail.com>
2019-11-02 21:11:14 -0400

    fix: Bug adding errors file
    Signed-off-by: Jake Schurch <jakeschurch@gmail.com>

---
+++ src/errors.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+#[derive(Debug, Fail)]
+pub enum InputError {
+    #[fail(display = "Could not parse toml: {:?}", err)]
+    InvalidToml { err: toml::de::Error },
+    #[fail(display = "Could not parse input: {:?}: ", err)]
+    BadInput { err: io::Error },
+}
+
+impl From<io::Error> for InputError {
+    fn from(err: io::Error) -> InputError {
+        InputError::InvalidToml { err }
+    }
+}
+
+impl From<toml::de::Error> for InputError {
+    fn from(err: toml::de::Error) -> InputError {
+        InputError::InvalidToml { err }
+    }
+}
